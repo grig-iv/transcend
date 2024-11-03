e944b2fd86b4f5e777e9275b361c0c3aad4f8831
grig-iv <abstractgrig@gmail.com>
2024-11-03 12:05:58 +0300

    change selected files map

--- app.go
+++ app.go
@@ -3,25 +3,25 @@ package main
 type app struct {
 	nav *nav
 
-	selectedFiles map[*file]struct{}
+	selectedFiles map[string]struct{}
 }
 
 func (a *app) init() {
 	a.nav = &nav{}
 	a.nav.init()
 
-	a.selectedFiles = make(map[*file]struct{})
+	a.selectedFiles = make(map[string]struct{})
 }
 
 func (a *app) toggleSelection(file *file) {
 	if a.isSelected(file) {
-		delete(a.selectedFiles, file)
+		delete(a.selectedFiles, file.path)
 	} else {
-		a.selectedFiles[file] = struct{}{}
+		a.selectedFiles[file.path] = struct{}{}
 	}
 }
 
 func (a *app) isSelected(file *file) bool {
-	_, ok := a.selectedFiles[file]
+	_, ok := a.selectedFiles[file.path]
 	return ok
 }

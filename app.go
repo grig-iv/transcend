d09aac58cd2b0865c14ca01e9d384cff8634ed9b
grig-iv <abstractgrig@gmail.com>
2024-11-04 02:35:20 +0300

    move copy to app

--- app.go
+++ app.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"os"
 	"path"
@@ -95,4 +96,32 @@ func (a *app) deleteSelected() {
 		}
 		delete(a.selectedPaths, p)
 	}
+}
+
+func copyFile(srcPath, dstPath string) error {
+	srcFile, err := os.OpenFile(srcPath, os.O_RDONLY, 0)
+	if err != nil {
+		return err
+	}
+
+	defer srcFile.Close()
+
+	srcStat, err := srcFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	dstFile, err := os.OpenFile(dstPath, os.O_WRONLY|os.O_CREATE|os.O_EXCL, srcStat.Mode())
+	if err != nil {
+		return err
+	}
+
+	defer dstFile.Close()
+
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
